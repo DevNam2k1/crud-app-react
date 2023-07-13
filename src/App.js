e00984f288467d1e5b450f448c45f355fe3ec01c
nam.nghiem <nam.nghiem@savvycomsoftware.com>
2023-07-13 16:43:43 +0700

    Feat ✨:  Get Api TableUsers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.scss';
 import Header from './components/Header';
 import TableUsers from './components/TableUsers';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <>
       <div className='app-container'>
-        <Header/>
-        <TableUsers/>
+        <Header />
+        <Container>
+          <TableUsers />
+        </Container>
       </div>
     </>
   );
